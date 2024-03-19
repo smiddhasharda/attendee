08d898641bc9835f192d5e65620fd787b379fd54
devsaxena012 <93965007+devsaxena012@users.noreply.github.com>
2024-03-19 12:55:28 +0530

    Push Updated Changes

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [
-      '@babel/preset-react', // Add this line to enable JSX transformation
-      '@babel/preset-flow', // Add this line to enable Flow syntax
-    ],
-    plugins: [
-      // Your existing plugins here
-      '@babel/plugin-proposal-export-namespace-from',
-      'react-native-reanimated/plugin',
-    ],
+    presets: ['babel-preset-expo'],
   };
 };
