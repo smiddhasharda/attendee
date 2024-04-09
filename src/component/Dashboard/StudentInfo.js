001307b4ae57a9264077c833d329d41c09ede3e9
Medha Yadav <medha.yadav@shardatech.org>
2024-04-09 16:23:37 +0530

    merge code

--- src/component/Dashboard/StudentInfo.js
+++ src/component/Dashboard/StudentInfo.js
@@ -1,11 +1,12 @@
 import {React,useState,useEffect} from 'react';
-import { View, Text, FlatList, StyleSheet, Dimensions ,ScrollView,Image,TextInput,ActivityIndicator} from 'react-native';
+import { View, Text, FlatList, StyleSheet, Dimensions ,ScrollView,Image,TextInput,ActivityIndicator, Pressable} from 'react-native';
 import { Ionicons } from '@expo/vector-icons'
 import user from '../../local-assets/userimg.jpg'
 
 const StudentInfo = () => {
   return (
-    <View style={styles.container}>   
+    <ScrollView >
+      <View style={styles.container}>   
         <View style={styles.studentInfoWrap}>
           <Text style={styles.infoHeader}>Student Info:</Text> 
           <View style={styles.container}>
@@ -85,19 +86,61 @@ const StudentInfo = () => {
               </View>
             </View>
           </View>
+        </View>     
+        <View>
+         <Text style={styles.infoHeader}>Course Info:</Text> 
+
+         
         </View>
-  </View>
+        {/* <View style={styles.studentInfoWrap}>
+      <View style={[styles.row,]}>
+        <Text style={[styles.label,{marginRight:4}]}>Copy Number:</Text>
+        <TextInput style={[styles.input,{marginRight:10}]} placeholder="Enter Copy Number"  editable={false} />
+          <Ionicons  name="barcode-sharp" size={24} color="black"  ></Ionicons>
+          <Ionicons name="stop-circle" size={24} color="black" ></Ionicons>
+      </View>   
+  <Text style={styles.label}>Alternate Sheet 1</Text>
+    <Text  style={styles.label}>Alternate Sheet 2</Text>
+      <View style={[styles.row,{justifyContent:"right"}]} >
+          <Ionicons name="add" size={24} color="black" ></Ionicons>
+          </View>
+        </View> */}
+
+       <View style={styles.studentInfoWrap}>
+            <Pressable style={styles.addButton}>
+              <Text style={styles.addButtonText}>Add Copies</Text>
+            </Pressable>
+            <View style={styles.table}>
+              <View style={styles.row}>
+                <Text style={[styles.cell, styles.header]}>Copy</Text>
+                <View style={styles.inputContainer}>
+                  <TextInput style={styles.input} placeholder="Enter Copy Number" editable={false} />
+                  <Text style={styles.orText}>OR</Text>
+                  <Ionicons name="barcode-sharp" size={24} color="black" />
+                  <Ionicons name="stop-circle" size={24} color="black" />
+                  <Ionicons name="add" size={24} color="red" />
+                </View>
+              </View>
+              <View style={styles.row}>
+                <Text style={[styles.cell, styles.header]}>Supplymentry 1</Text>
+                <View style={styles.inputContainer}>
+                  <TextInput style={styles.input} placeholder="Enter Copy Number" editable={false} />       
+                  <Ionicons name="add" size={24} color="red" />
+                </View>
+              </View>
+            </View>
+       </View>
+      </View>
+  </ScrollView>
   )
 }
 
 export default StudentInfo
 
-
-
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor:"#fff" ,
+      backgroundColor:"#fff" , 
     },
     studentInfoWrap: {
       fontSize: 16,
@@ -108,6 +151,7 @@ const styles = StyleSheet.create({
       marginBottom: 15,
       borderRadius: 8,
       width: 100 + '%'
+      // width: Dimensions.get("window").width / 1 - 40,
     },
     infoHeader: {
       fontSize: 18,
@@ -116,7 +160,7 @@ const styles = StyleSheet.create({
       padding: 10
     },
     studentInfoRow: {
-      flexDirection: 'row', 
+      flexDirection: 'column', 
       height: 100, 
       padding: 20
     },
@@ -146,4 +190,76 @@ const styles = StyleSheet.create({
       fontSize: 16,
       marginLeft: 5,
     },
+    container: {
+      flex: 1,
+      padding: 10,
+      // justifyContent: 'center',
+      // alignItems: 'center',
+      },
+      table: {
+      borderWidth: 1,
+      borderColor: '#ddd',
+      borderRadius: 10,
+      overflow: 'hidden',
+      marginBottom: 20,
+      backgroundColor: '#f9f9f9',
+      shadowColor: '#000',
+      shadowOffset: {
+      width: 0,
+      height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 3.84,
+      elevation: 5,
+      // width: '100%',
+      },
+      row: {
+      flexDirection: 'row',
+      borderBottomWidth: 1,
+      borderColor: '#ddd',
+      alignItems: 'center',
+      paddingHorizontal: 10,
+      },
+      cell: {
+      flex: 1,
+      paddingVertical: 10,
+      paddingHorizontal: 8,
+      color: '#333',
+      },
+      header: {
+      fontWeight: 'bold',
+      backgroundColor: '#f0f0f0',
+  
+      },
+      inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      // marginLeft: 'auto', 
+      },
+      input: {
+      flex: 1,
+      padding: 8,
+      borderWidth: 1,
+      borderColor: '#ddd',
+      borderRadius: 5,
+      marginRight: 10,
+      backgroundColor: '#fff',
+      },
+
+      orText: {
+      marginRight: 10,
+      },
+      addButton:{
+        padding:10,
+        borderRadius:5,
+        backgroundColor:"#129912",
+        width:100,
+        marginBottom:10,
+        justifyContent:"right",
+      
+      },
+      addButtonText:{
+        color:"#fff"
+      },
+    
   });
