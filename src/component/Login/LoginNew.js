b7e5a28bafe7a86a2d60ee3659862f300092b7c1
Medha Yadav <medha.yadav@shardatech.org>
2024-05-01 11:11:15 +0530

    merge code

--- src/component/Login/LoginNew.js
+++ src/component/Login/LoginNew.js
@@ -1,89 +1,71 @@
-import React from 'react';
-import { ScrollView, ImageBackground, StyleSheet, Text, View, Dimensions, TextInput, Pressable } from 'react-native';
+import React,{useState} from 'react';
+import { ScrollView, ImageBackground, StyleSheet, Text, View, Dimensions, TextInput, Pressable,Button,Image ,Platform,} from 'react-native';
 import loginBackground from '../../local-assets/attendlogin.jpg';
 
-const { width, height } = Dimensions.get('window');
-const isMobile = width < 768;
-
 function LoginNew() {
-  return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-      <View style={styles.container}>
-        <View style={styles.imageContainer}>
-          <ImageBackground source={loginBackground} style={styles.imageBackground}/>
-        </View>
-        <View style={styles.formContainer}>
-          <Text style={styles.heading}>Login To Attendee</Text>
-          <View style={styles.emailInput}>
-            <Text style={styles.label}>Email:</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter the Email"
-            />
-          </View>
-          <Pressable style={styles.button} onPress={() => alert("OTP sent!")}>
-            <Text style={styles.buttonText}>Send OTP</Text>
-          </Pressable>
-        </View>
+  return ( 
+   <View style={styles.container}>
+      <View style={styles.form}>
+      <Image style={styles.image} source= {require("../../local-assets/attendlogin.jpg")} />
+      <Text style={styles.label}>Email Id</Text>
+      <TextInput  style={styles.input} placeholder='Enter Your Email ID'/>
+      <Button title='Send Otp' />
       </View>
-    </ScrollView>
+   </View>    
   );
 }
 
 export default LoginNew;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
+
+  container:{
+    flex:1,
+    // justifyContent:"center",
+    paddingHorizontal:20,
+    paddingVertical:20,
+    backgroundColor:"#f5f5f5",
   },
-  imageContainer: {
-    flex: isMobile ? 1 : 0.5,
+
+  form:{
+    backgroundColor:"white",
+    padding:20,
+    borderRadius:10,
+    // shadowColor:"black",
+    // shadowOffset:{
+    //   width:0,
+    //   height:2
+    // }
   },
-  formContainer: {
-    flex: 1,
-    padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
+  label:{
+    fontSize:16,
+    marginBottom:5,
+    fontWeight:"bold"
   },
-  imageBackground: {
-    flex: 1,
-    width: '100%',
-    resizeMode: 'cover',
+
+  input:{
+   height:40,
+   borderColor:"#ddd",
+   borderWidth:1,
+   marginBottom:15,
+   padding:10,
+   borderRadius:5,
   },
-  emailInput:{
-    flexDirection: "row",
-    alignItems: 'center',
-    marginBottom: 20,
-  },
+ 
   heading:{
     fontSize: 25,
     fontWeight: "bold",
-    marginBottom: 20,
+    marginBottom: 5,
   },
-  label: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginRight: 10,
+  image:{
+    width:200,
+    height:200,
+    alignSelf:"center",
+    marginBottom:20,
   },
-  input: {
-    flex: 1,
-    height: 40,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
+  
+  headingtext:{
+  paddingVertical:10,
   },
-  button: {
-    width: '100%',
-    padding: 15,
-    backgroundColor: '#ef1313',
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  }
+
 });
