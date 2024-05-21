724e6b2d0b593657a8d6f422434002faa0214935
devsaxena012 <93965007+devsaxena012@users.noreply.github.com>
2024-05-21 11:01:07 +0530

    Push Changes

--- src/component/Student/StudentInfo.js
+++ src/component/Student/StudentInfo.js
@@ -923,10 +923,7 @@ const styles = StyleSheet.create({
   },
   mainCopyText: {
     color: "#000",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
+    boxShadow: '0px 2px 3.84px rgba(0, 0, 0, 0.25)',
     fontWeight: "bold",
   },
 });
--- src/globalComponent/ToastContainer/ToastContainer.style.js
+++ src/globalComponent/ToastContainer/ToastContainer.style.js
@@ -12,14 +12,8 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     ...Platform.select({
       ios: {
-        shadowColor: "#000",
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
+        boxShadow: '0px 2px 3.84px rgba(0, 0, 0, 0.25)',
+        elevation: 5, 
       },
       android: {
         elevation: 5,
@@ -33,13 +27,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     ...Platform.select({
       ios: {
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
+        boxShadow: '0px 2px 3.84px rgba(0, 0, 0, 0.25)',
         elevation: 5,
       },
       android: {
